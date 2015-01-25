2ff5b540e74f30fef7cc3685080fbfc6b0a1c362
Weston Dransfield <weston@CCPSs-MacBook-Pro.local>
2015-01-24 17:29:00 -0700

    test

--- results.php
+++ results.php
@@ -58,7 +58,7 @@ saveData();
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
-	<script src="/assets/chart.min.js"></script>
+	<script src="assets/chart.min.js"></script>
 </head>
 
 <body>
