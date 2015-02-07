ef43fb25e6d92e6d1f3f6567456866559a56493f
Weston Dransfield <weston@CCPSs-MacBook-Pro.local>
2015-02-07 16:00:31 -0700

    test db.

--- goalie.php
+++ goalie.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION["signed-in"]))
 	$_SESSION['email'] = $_POST['email'];
 
 	//get the current user
-	$stmt = $db->prepare("SELECT first_name, last_name FROM user WHERE email=:email ");
+	$stmt = $db->prepare("SELECT * FROM user WHERE email=:email ");
 	$stmt->execute(array(':email' => $_SESSION['email']));
 	$user = $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
@@ -56,7 +56,7 @@ if (!isset($_SESSION["signed-in"]))
 			<div class="navbar-collapse collapse" id="navbar-main">
 				<ul class="nav navbar-nav navbar-right">
 					<li><a href="goalie-new-goal.php">Set a New Goal</a></li>
-					<li><a href="goalie-settings.php"><?php echo $user[0]['first_name'] ?>'s Settings</a></li>
+					<li><a href="goalie-settings.php"><?php //echo $user[0]['first_name'] ?>'s Settings</a></li>
 					<li><a href="https://wrapbootstrap.com/?ref=bsw" target="_blank">Logout</a></li>
 				</ul>
 
@@ -90,13 +90,7 @@ if (!isset($_SESSION["signed-in"]))
 
 		<div>
 			
-			<?php 
-				
-
-				echo "statement: " . $stmt;
-				echo "user: " . $user;
-				echo "session: " . $_SESSION['email'];
-			 ?>
+			
 
 		</div>
 
