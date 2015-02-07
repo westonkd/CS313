9ebcb61f6b255a6a5a9b34793b4d9e9b5397fa42
Weston Dransfield <weston@CCPSs-MacBook-Pro.local>
2015-02-07 14:45:11 -0700

    test db.

--- goalie-signin.php
+++ goalie-signin.php
@@ -1,4 +1,8 @@
-<?php  ?>
+<?php  
+	session_start();
+
+    
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -24,12 +28,12 @@
 			<div class="col-all-12">
 				<div class="sign-in">
 					<h1>Goalie Sign In</h1>
-					<form class="form-horizontal">
+					<form class="form-horizontal" action="goalie.php">
 						<fieldset>
 							<div class="form-group">
 								<label for="inputEmail" class="col-lg-2 control-label">Email</label>
 								<div class="col-lg-10">
-									<input type="text" class="form-control" id="inputEmail" placeholder="Email">
+									<input name="email" type="text" class="form-control" id="inputEmail" placeholder="Email">
 								</div>
 							</div>
 							<div class="form-group">
