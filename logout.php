1c6abfa670272c3b2414dbabcb02a7f188b9aa25
Weston Dransfield <weston@CCPSs-MacBook-Pro.local>
2015-02-07 17:39:31 -0700

    session test

--- logout.php
+++ logout.php
@@ -1,4 +1,18 @@
 <?php 
-	session_destroy();
-	header( 'Location: /goalie-signin.php');
- ?>
+session_start();
+
+// Unset all of the session variables.
+$_SESSION = array();
+
+// If it's desired to kill the session, also delete the session cookie.
+if (ini_get("session.use_cookies")) {
+	$params = session_get_cookie_params();
+	setcookie(session_name(), '', time() - 42000,
+		$params["path"], $params["domain"],
+		$params["secure"], $params["httponly"]
+		);
+}
+
+//destroy the session.
+session_destroy();
+?>
