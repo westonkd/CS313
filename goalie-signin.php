80b04499d73a6f46b7ebe420eee956532c4d1f63
Weston Dransfield <weston@CCPSs-MacBook-Pro.local>
2015-02-07 17:14:07 -0700

    session test

--- goalie-signin.php
+++ goalie-signin.php
@@ -1,5 +1,5 @@
 <?php  
-	session_start();
+session_start();
 
 	//if the user is already signed in
 	if (isset($_SESSION["signed-in"]))
--- goalie.php
+++ goalie.php
@@ -1,4 +1,4 @@
-<?php  
+<?php 
 session_start();
 
 	//if the user is already signed in
