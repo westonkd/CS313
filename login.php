4ba4cb272cd8260a9304ff33c80f63ba30c4601c
Weston Dransfield <weston@CCPSs-MacBook-Pro.local>
2015-02-14 07:43:08 -0700

    test

--- login.php
+++ login.php
@@ -23,7 +23,7 @@ if (isset($_SESSION["signed-in"])) {
 			$_SESSION['email'] = $_POST['email'];
 			$_SESSION['signed-in'] = true;
 
-			//header( 'Location: /goalie.php' );
+			header( 'Location: /goalie.php' );
 		} 
 	} 
 		
