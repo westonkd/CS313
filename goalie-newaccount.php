d2df67ba2b3ccb4bfa1e817511b67204a4ce28d5
Weston Dransfield <weston@CCPSs-MacBook-Pro.local>
2015-02-14 08:30:16 -0700

    test

--- goalie-newaccount.php
+++ goalie-newaccount.php
@@ -7,5 +7,30 @@ $lastName = $_POST['lastName'];
 $password = $_POST['password'];
 $rePassword = $_POST['reenter'];
 
+//connect to the DB
+require("dbConnector.php");
+$db = loadDatabase();
+
+//if the passwords don't match or the email address is already taken
+if (!$password == $rePassword)
+{
+	header('Location: goalie-signin.php');
+}
+
+//current date
+$date = date('Y-m-d H:i:s');
+
+//insert the new user
+$statement = $db->prepare('INSERT INTO user(first_name, last_name, email, password, last_visited) VALUES(:first, :last, :email, :password, :lastVisit)');
+
+$statement->bindParam(':first', $firstName);
+$statement->bindParam(':last', $lastName);
+$statement->bindParam(':email', $email);
+$statement->bindParam(':password', $password);
+$statement->bindParam(':lastVisit', $date);
+$statement->execute();
+
+echo "Success";
+
 echo "working";
- ?>
+?>
