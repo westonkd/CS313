a9a0e0fa8249507dd63e1b83c5775453d6e9349c
Weston Dransfield <weston@CCPSs-MacBook-Pro.local>
2015-02-14 08:11:55 -0700

    test

---
+++ goalie-newaccount.php
@@ -0,0 +1,11 @@
+<?php 
+session_start();
+
+$email = $_POST['email'];
+$firstName = $_POST['firstName'];
+$lastName = $_POST['lastName'];
+$password = $_POST['password'];
+$rePassword = $_POST['reenter'];
+
+echo "working";
+ ?>
