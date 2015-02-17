d85c4a500a2a19172be0a636024703fcfaeb75b0
Weston Dransfield <weston@CCPSs-MacBook-Pro.local>
2015-02-16 20:09:33 -0700

    test

--- goalie-updategoal.php
+++ goalie-updategoal.php
@@ -8,7 +8,7 @@ if (isset($_SESSION['email']) && !empty($_POST['days-completed'])) {
 	$days = $_POST['days-completed'];
 
 	//move the current goal to history
-	// $stmt = $db->prepare("UPDATE goal INNER JOIN user ON goal.user_id = user.id WHERE email=:email AND is_current_goal=1");
+	// $stmt = $db->prepare("UPDATE goal INNER JOIN user ON goal.user_id = user.id SET goal.days_complete = 00000 WHERE email=:email AND is_current_goal=1");
 	// $stmt->execute(array(':email' => $_SESSION['email']));
 
 	//move the current goal to history
@@ -16,10 +16,12 @@ if (isset($_SESSION['email']) && !empty($_POST['days-completed'])) {
 	$stmt->execute(array(':email' => $_SESSION['email']));
 	$currentGoal = $stmt->fetch();
 
+	$newDaysSet = $currentGoal['days_complete'];
 	foreach($days as $day) {
-            echo $day."\n";
+        $newDaysSet[(int) $day] = "1";
     }
 
+    echo $newDaysSet."\n";
 	echo "in: ".$currentGoal['days_complete'];
 
 } 
