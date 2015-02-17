e982454140982e4cbc7bc5af2065165184399061
Weston Dransfield <weston@CCPSs-MacBook-Pro.local>
2015-02-16 18:24:39 -0700

    test

--- goalie-creategoal.php
+++ goalie-creategoal.php
@@ -1,6 +1,13 @@
 <?php 
 session_start();
 
+//if the user is already signed in
+if (!isset($_SESSION["signed-in"]))
+{
+	//redirect to the main page
+	header( 'Location: /goalie-signin.php' );
+}
+
 $email = $_POST['email'];
 $firstName = $_POST['firstName'];
 $lastName = $_POST['lastName'];
@@ -20,9 +27,7 @@ $stmt->execute(array(':email' => $_SESSION['email']));
 $user = $stmt->fetch();
 
 //move the current goal to history
-$stmt = $db->prepare("UPDATE goal INNER JOIN user ON goal.user_id = user.id SET goal.is_current_goal = 0, goal.date_to_finish = :finish WHERE email=:email AND is_current_goal=1");
-$stmt->execute(array(':email' => $_SESSION['email'], ':finish' => $date));
-$currentGoal = $stmt->fetch();
+require("goalie-deletegoal.php");
 
 //7 days from date
 $plusOneWeek = strtotime('+1 Week');
@@ -31,6 +36,9 @@ $plusOneWeek = strtotime('+1 Week');
 if(isset($_POST['goalName'])) {
 	$statement = $db->prepare("INSERT INTO goal(title, description, date_set, date_to_finish, percent_complete, last_updated, is_current_goal, user_id) VALUES(:title, :description, :date_set, :date_to_finish, :percent_complete, :last_updated, :is_current_goal, :user_id)");
 	$goalWasCreated = $statement->execute(array(':title' => $_POST['goalName'], ':description' => $_POST['goalDescription'], ':date_set' => $date, ':date_to_finish' => $plusOneWeek, ':percent_complete' => 0, ':last_updated' => $date, ':is_current_goal' => 1, ':user_id' => $user[id]));
-}	
+}
+
+//go to the main page
+header( 'Location: /goalie-signin.php' );
 
 ?>
---
+++ goalie-deletegoal.php
@@ -0,0 +1,4 @@
+//move the current goal to history
+$stmt = $db->prepare("UPDATE goal INNER JOIN user ON goal.user_id = user.id SET goal.is_current_goal = 0, goal.date_to_finish = :finish WHERE email=:email AND is_current_goal=1");
+$stmt->execute(array(':email' => $_SESSION['email'], ':finish' => $date));
+$currentGoal = $stmt->fetch();
