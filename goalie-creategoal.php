51fc1ad82b51cb52880a7ce59212ca064dc034fe
Weston Dransfield <weston@CCPSs-MacBook-Pro.local>
2015-02-16 18:08:17 -0700

    test

--- goalie-creategoal.php
+++ goalie-creategoal.php
@@ -11,42 +11,26 @@ $rePassword = $_POST['reenter'];
 require("dbConnector.php");
 $db = loadDatabase();
 
+//current date
+$date = date('Y-m-d H:i:s');
+
 //get the current user
 $stmt = $db->prepare("SELECT * FROM user WHERE email=:email ");
 $stmt->execute(array(':email' => $_SESSION['email']));
 $user = $stmt->fetch();
 
 //move the current goal to history
-$stmt = $db->prepare("UPDATE goal INNER JOIN user ON goal.user_id = user.id SET goal.is_current_goal = 0 WHERE email=:email AND is_current_goal=1");
+$stmt = $db->prepare("UPDATE goal INNER JOIN user ON goal.user_id = user.id SET goal.is_current_goal = 0, goal.date_to_finish = $date WHERE email=:email AND is_current_goal=1");
 $stmt->execute(array(':email' => $_SESSION['email']));
 $currentGoal = $stmt->fetch();
 
-//current date
-$date = date('Y-m-d H:i:s');
+//7 days from date
 $plusOneWeek = strtotime('+1 Week');
 
 //insert the new goal
 if(isset($_POST['goalName'])) {
 	$statement = $db->prepare("INSERT INTO goal(title, description, date_set, date_to_finish, percent_complete, last_updated, is_current_goal, user_id) VALUES(:title, :description, :date_set, :date_to_finish, :percent_complete, :last_updated, :is_current_goal, :user_id)");
 	$goalWasCreated = $statement->execute(array(':title' => $_POST['goalName'], ':description' => $_POST['goalDescription'], ':date_set' => $date, ':date_to_finish' => $plusOneWeek, ':percent_complete' => 0, ':last_updated' => $date, ':is_current_goal' => 1, ':user_id' => $user[id]));
-	echo "Goal: " . $goalWasCreated;
 }	
-
-
-
-// //insert the new user
-// $statement = $db->prepare('INSERT INTO user(first_name, last_name, email, password, last_visited, pref_id) VALUES(:first, :last, :email, :password, :dateVisit, :pref)');
-
-// $statement->bindParam(':first', $firstName);
-// $statement->bindParam(':last', $lastName);
-// $statement->bindParam(':email', $email);
-// $statement->bindParam(':password', $password);
-// $statement->bindParam(':dateVisit', $date);
-// $statement->bindParam(':pref', $statementID);
-// $wasSuccesful = $statement->execute();
-
-//header('Location: goalie-signin.php');
-
-echo $user['id'];
 
 ?>
