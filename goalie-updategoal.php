81c0be6f8e3d7a54829b8e344dc3e0b9506440b8
Weston Dransfield <weston@CCPSs-MacBook-Pro.local>
2015-02-16 19:49:15 -0700

    test

--- goalie-updategoal.php
+++ goalie-updategoal.php
@@ -3,9 +3,7 @@ session_start();
 
 if (isset($_SESSION['email'])) {
 
-	//move the current goal to history
-	$stmt = $db->prepare("UPDATE goal INNER JOIN user ON goal.user_id = user.id WHERE email=:email AND is_current_goal=1");
-	$stmt->execute(array(':email' => $_SESSION['email']));
+	
 
 	//move the current goal to history
 	$stmt = $db->prepare("SELECT * FROM goal INNER JOIN user ON goal.user_id = user.id WHERE email=:email AND is_current_goal=1");
