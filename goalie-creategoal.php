ae66cdfe9d2eb466e6103b68a49c1025c3aa8149
Weston Dransfield <weston@CCPSs-MacBook-Pro.local>
2015-02-16 18:14:17 -0700

    test

--- goalie-creategoal.php
+++ goalie-creategoal.php
@@ -20,8 +20,8 @@ $stmt->execute(array(':email' => $_SESSION['email']));
 $user = $stmt->fetch();
 
 //move the current goal to history
-$stmt = $db->prepare("UPDATE goal INNER JOIN user ON goal.user_id = user.id SET goal.is_current_goal = 0, goal.date_to_finish = $date WHERE email=:email AND is_current_goal=1");
-$stmt->execute(array(':email' => $_SESSION['email']));
+$stmt = $db->prepare("UPDATE goal INNER JOIN user ON goal.user_id = user.id SET goal.is_current_goal = 0, goal.date_to_finish = :finish WHERE email=:email AND is_current_goal=1");
+$stmt->execute(array(':email' => $_SESSION['email'], ':finish' => $date));
 $currentGoal = $stmt->fetch();
 
 //7 days from date
