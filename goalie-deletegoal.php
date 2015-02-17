441d3eaaf069aa373261492f9fece7f0605c5a9d
Weston Dransfield <weston@CCPSs-MacBook-Pro.local>
2015-02-16 18:25:33 -0700

    test

--- goalie-deletegoal.php
+++ goalie-deletegoal.php
@@ -1,4 +1,6 @@
-//move the current goal to history
-$stmt = $db->prepare("UPDATE goal INNER JOIN user ON goal.user_id = user.id SET goal.is_current_goal = 0, goal.date_to_finish = :finish WHERE email=:email AND is_current_goal=1");
-$stmt->execute(array(':email' => $_SESSION['email'], ':finish' => $date));
-$currentGoal = $stmt->fetch();
+<?php 
+	//move the current goal to history
+	$stmt = $db->prepare("UPDATE goal INNER JOIN user ON goal.user_id = user.id SET goal.is_current_goal = 0, goal.date_to_finish = :finish WHERE email=:email AND is_current_goal=1");
+	$stmt->execute(array(':email' => $_SESSION['email'], ':finish' => $date));
+	$currentGoal = $stmt->fetch();	
+ ?>
